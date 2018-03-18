4208dbccad6f8e0adbbc1a5e840a1249d25c4c3f
Rafael Vales <rafaelvalesb@gmail.com>
2018-03-17 22:22:44 -0300

    Update t1.hs

--- t1/t1.hs
+++ t1/t1.hs
@@ -1,13 +1,14 @@
--- haskell02
+-- Prática: haskell02
 
--- 1
+-- 1. Crie uma função isVowel :: Char -> Bool que verifique se um caracter é uma vogal ou não.
 isVowel :: Char -> Bool
 isVowel c = c `elem` "aeiouAEIOU"
--- 2
+
+-- 2. Escreva uma função addComma, que adicione uma vírgula no final de cada string contida numa lista.
 addComma :: [String] -> [String]
 addComma s = map (++ ",") s;
 
--- 3
+-- 3. Crie uma função htmlListItems :: [String] -> [String], que receba uma lista de strings e retorne outra lista contendo as strings formatadas como itens de lista em HTML.
 concatenar3 :: String -> String
 concatenar3 s = "<LI>" ++ s ++ "</LI>"
 
@@ -17,14 +18,14 @@ htmlListItems1 s = map (concatenar3) s
 htmlListItems2 :: [String] -> [String]
 htmlListItems2 s = map (\x -> "<LI>" ++ x ++ "</LI>") s
 
--- 4
+-- 4. Defina uma função que receba uma string e produza outra retirando as vogais, conforme os exemplos abaixo.
 semVogais :: [Char] -> [Char]
 semVogais s = filter (not.isVowel) s
 
 semVogais2 :: [Char] -> [Char]
 semVogais2 s = filter (\x -> not (x `elem` "aeiouAEIOU")) s
 
--- 5
+-- 5. Defina uma função que receba uma string, possivelmente contendo espaços, e que retorne outra string substituindo os demais caracteres por '-', mas mantendo os espaços.
 ehEspaco :: Char -> Char
 ehEspaco c = if c == ' ' then ' ' else '-'
 
@@ -34,8 +35,8 @@ trocaEspaco s = map (ehEspaco) s
 trocaEspaco2 :: String -> String
 trocaEspaco2 s = map (\x -> if x /= ' ' then '-' else ' ') s
 
--- 6
+-- 6. Escreva uma função firstName :: String -> String que, dado o nome completo de uma pessoa, obtenha seu primeiro nome. Suponha que cada parte do nome seja separada por um espaço e que não existam espaços no início ou fim do nome.
 firstName :: String -> String
 firstName s = takeWhile(/= ' ') s
 
--- 7
+-- 7. Escreva uma função isInt :: String -> Bool que verifique se uma dada string só contém dígitos de 0 a 9.
