2c865678cf52ba1eb2c4252a1e2247fda95de224
Rafael Vales <rafaelvalesb@gmail.com>
2018-03-21 15:38:40 -0300

    Add exercício 02

--- t2/t2.hs
+++ t2/t2.hs
@@ -1,4 +1,9 @@
--- 1 --
+-- 1. Usando recursão, escreva uma função geraTabela :: Int -> [(Int,Int)] que produza uma lista com n tuplas, cada tupla com números de n a 1 e seus respectivos quadrados.
 geraTabela :: Int -> [(Int,Int)]
 geraTabela 0 = []
 geraTabela x = (x,x*x):(geraTabela (x-1))
+
+-- 2. Defina uma função recursiva que verifique se um dado caracter está contido numa string.
+contido :: Char -> [Char] -> Bool
+contido _ [] = False
+contido c (x:xs) = if (x == c) then True else contido c xs
