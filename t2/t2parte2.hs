f1baefe7ecfc1c1e85b332a74b48b64bfa682f6f
Rafael Vales <rafaelvalesb@gmail.com>
2018-03-24 19:51:40 -0300

    Add exercício 02

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -1,6 +1,9 @@
---  Trabalho 2 Parte 1  --
+--  Trabalho 2 Parte 2  --
 --     Rafael Vales     --
 
 isBin :: String -> Bool
 isBin [] = True
 isBin (x:xs) = if (x /= '0' && x /= '1') then False else isBin xs
+
+isBin' :: String -> Bool
+isBin' str = all (\x -> x == '0' || x == '1' ) str
