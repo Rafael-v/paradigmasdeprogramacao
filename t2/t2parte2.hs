ebdc43d3b06dbac6b181f049eb7605092f8cdf63
Rafael Vales <rafaelvalesb@gmail.com>
2018-03-24 20:24:36 -0300

    Add exercício 03

--- t2/t2parte2.hs
+++ t2/t2parte2.hs
@@ -7,3 +7,11 @@ isBin (x:xs) = if (x /= '0' && x /= '1') then False else isBin xs
 
 isBin' :: String -> Bool
 isBin' str = all (\x -> x == '0' || x == '1' ) str
+
+bin2dec :: [Int] -> Int
+bin2dec [] = undefined
+bin2dec bits = auxBin2Dec bits ((length bits)-1)
+
+auxBin2Dec :: [Int] -> Int -> Int
+auxBin2Dec [] _ = 0
+auxBin2Dec (x:xs) n = x*(2^n) + auxBin2Dec xs (n-1)
