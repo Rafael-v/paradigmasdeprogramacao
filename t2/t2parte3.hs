dbd42dd7f21b8b128a515e6a067bd4965576567b
Rafael Vales <rafaelvalesb@gmail.com>
2018-03-24 22:39:14 -0300

    Add função principal

--- t2/t2parte3.hs
+++ t2/t2parte3.hs
@@ -1,1 +1,9 @@
+--  Trabalho 2 Parte 3  --
+--     Rafael Vales     --
+
+-- Implementação de uma função isEanOk :: String -> Bool, que verifica se uma dada string representa um número EAN-13 com dígito verificador válido.
+
 isEanOk :: String -> Bool
+isEanOk str = if (length str) /= 13
+  then False
+  else (last str == calcCheckDigit str)
