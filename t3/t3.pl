a3dc37b741ea474cf693e73f5fc7d3a26b505845
Rafael Vales <rafaelvalesb@gmail.com>
2018-04-11 15:48:15 -0300

    Add 05

--- t3/t3.pl
+++ t3/t3.pl
@@ -7,7 +7,7 @@ has5(L) :- L = [_,_,_,_,_].
 % 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
 hasN(L,N) :- length(L,N).
 
-% 4 Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
+% 4. Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
 potN0(0,[1]).
 potN0(N,L) :-
     N > 0,
@@ -15,3 +15,12 @@ potN0(N,L) :-
     H is 2**N,
     N1 is N - 1,
     potN0(N1,T).
+
+% 5. Defina um predicado zipmult(L1,L2,L3), de forma que cada elemento da lista L3 seja o produto dos elementos de L1 e L2 na mesma posição do elemento de L3.
+zipmult([],[],[]).
+zipmult(L1,L2,L3) :-
+    L1 = [H1|T1],
+    L2 = [H2|T2],
+    L3 = [H3|T3],
+    H3 is H1 * H2,
+    zipmult(T1,T2,T3).
