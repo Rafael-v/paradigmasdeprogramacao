de3d698a04f17f435f2b5e8510a3f93d68d35209
Rafael Vales <rafaelvalesb@gmail.com>
2018-04-11 15:39:13 -0300

    Add 04

--- t3/t3.pl
+++ t3/t3.pl
@@ -6,3 +6,12 @@ has5(L) :- L = [_,_,_,_,_].
 
 % 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
 hasN(L,N) :- length(L,N).
+
+% 4 Defina um predicado potN0(N,L), de forma que L seja uma lista de potências de 2, com expoentes de N a 0.
+potN0(0,[1]).
+potN0(N,L) :-
+    N > 0,
+    L = [H|T],
+    H is 2**N,
+    N1 is N - 1,
+    potN0(N1,T).
