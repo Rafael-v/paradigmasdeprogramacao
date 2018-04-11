45cefd49c94226c67fc6faa865867cc2e8f66c9c
Rafael Vales <rafaelvalesb@gmail.com>
2018-04-11 15:17:34 -0300

    Add 02 e 03; modifica 0

--- t3/t3.pl
+++ t3/t3.pl
@@ -1,2 +1,8 @@
 % 1. Defina um predicado zeroInit(L) que é verdadeiro se L for uma lista que inicia com o número 0 (zero).
-zeroInit(L) :- nth0(0,L,X), X == 0.
+zeroInit(L) :- [0|_] = L.
+
+% 2. Defina um predicado has5(L) que é verdadeiro se L for uma lista de 5 elementos.
+has5(L) :- L = [_,_,_,_,_].
+
+% 3. Defina um predicado hasN(L,N) que é verdadeiro se L for uma lista de N elementos.
+hasN(L,N) :- length(L,N).
