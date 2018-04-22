e83f667698dc3d148bca25cef134d6768d6db439
Rafael Vales <rafaelvalesb@gmail.com>
2018-04-22 18:23:40 -0300

    Update arquivos.hs

--- extras/obi/arquivos.hs
+++ extras/obi/arquivos.hs
@@ -1,4 +1,4 @@
-/*
+{-
   Arquivos
   Aldo tem N arquivos em seu computador, cada um com um tamanho em bytes. Ele quer dividir estes arquivos
   em pastas, porém o sistema do computador é velho e só aceita pastas com as duas seguintes limitações:
@@ -14,11 +14,21 @@
   linha contém N inteiros indicando o tamanho de cada arquivo.
   Saída: Seu programa deve escrever uma única linha na saída, contendo um único número inteiro, a quantidade
   mínima possível de pastas.
-*/
+-}
+
+arquivos :: Int -> Int -> [Int] -> Int
+arquivos a b list = if (length list == a) then arqAux b b list else -1
+
+arqAux :: Int -> Int -> [Int] -> Int
+arqAux _ _ [] = 0
+arqAux b t list = if (t `elem` list) then 1 + arqAux b t (removeArq b t list) else arqAux b (t-1) list
+
+removeArq :: Int -> Int -> [Int] -> [Int]
+removeArq b t list = remove (verif (b-t) list) (remove t list)
 
 verif :: Int -> [Int] -> Int
 verif 0 _ = 0
-verif x list = if (length list == length (filter (/=x) list)) then x else verif (x-1) list
+verif x list = if (x `elem` list) then x else verif (x-1) list
 
 remove :: Int -> [Int] -> [Int]
 remove _ [] = []
