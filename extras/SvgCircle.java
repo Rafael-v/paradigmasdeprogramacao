af43bd5d34c713997ce533b65af8f90f8c35f05b
Rafael Vales <rafaelvalesb@gmail.com>
2018-05-09 21:33:10 -0300

    Add círculos aleatórios

--- extras/SvgCircle.java
+++ extras/SvgCircle.java
@@ -1,5 +1,7 @@
 import java.io.*;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class SvgCircle {    
     private double x, y, r;
@@ -14,24 +16,34 @@ public class SvgCircle {
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        ArrayList<SvgCircle> circs = new ArrayList<SvgCircle>();
         
         System.out.print("Digite altura e largura da tela: ");
-        double altura = in.nextDouble();
-        double largura = in.nextDouble();
-        
-        System.out.print("Deseja inserir quantos circulos? ");
-        int totalCircs = in.nextInt();
-        SvgCircle[] circs = new SvgCircle[totalCircs];
-        System.out.println("Digite, nessa ordem, a posicao x, y, raio e cor de cada circulo: ");
-        for (int i = 0; i < circs.length; i++) {
-            System.out.print("Circulo " + (i+1) + ": ");
-            circs[i] = new SvgCircle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.next());
-        }
-        
+        int altura = in.nextInt();
+        int largura = in.nextInt();
+
+        lerCirculos(altura, largura, circs);
         geraArquivoSvg(altura, largura, circs);
     }
     
-    private static void geraArquivoSvg(double altura, double largura, SvgCircle[] circs) {
+    private static void lerCirculos(int altura, int largura, ArrayList<SvgCircle> circs) {
+        Scanner in = new Scanner(System.in);
+        Random rand = new Random();
+        String colors[] = {"fuchsia","blue","yellow","aqua","lime","black","red","white"};
+
+        System.out.println("Digite, nessa ordem, a posicao x, y, raio e cor de cada circulo: ");
+        do {
+            circs.add(new SvgCircle(in.nextDouble(), in.nextDouble(), in.nextDouble(), in.next()));
+            System.out.print("Deseja inserir um novo circulo? (s/n) ");
+        } while (in.next().charAt(0) != 'n');
+
+        System.out.print("Inserir mais circulos randomicamente? Digite a quantidade: ");
+        int totalRandom = in.nextInt();
+        for (int i = 0; i < totalRandom; i++)
+            circs.add(new SvgCircle(rand.nextInt(largura), rand.nextInt(altura), rand.nextInt(largura/5), colors[rand.nextInt(colors.length)]));
+    }
+
+    private static void geraArquivoSvg(int altura, int largura, ArrayList<SvgCircle> circs) {
         try {
             File file = new File("svg.txt");
             if (!file.exists())
