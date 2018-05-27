d529840aebe0d30c24b4ba8cd2336966bc4aeb22
Rafael Vales <rafaelvalesb@gmail.com>
2018-05-27 00:24:05 -0300

    Add função para gerar o Shape

--- t5/Vertex.java
+++ t5/Vertex.java
@@ -1,48 +1,33 @@
-import javafx.application.Application;
-import javafx.scene.input.MouseEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.shape.Circle;
-import javafx.scene.shape.Rectangle;
-import javafx.scene.shape.Polygon;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.*;
-import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.layout.VBox;
-import javafx.scene.control.Button;
-import javafx.scene.layout.HBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.ToolBar;
-import javafx.scene.control.ColorPicker;
-import javafx.geometry.Pos;
-import javafx.geometry.Insets;
-import javafx.application.Application;
-import javafx.event.*;
-import javafx.scene.Scene;
-import javafx.scene.control.ColorPicker;
-import javafx.geometry.Insets;
-import javafx.scene.layout.HBox;
-import javafx.scene.paint.Color;
-import javafx.scene.text.*;
-import javafx.stage.Stage;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TextInputDialog;
-import javafx.scene.control.ButtonType;
-import java.util.Optional;
-import java.util.ArrayList;
 
 public class Vertex {
     public double x, y;
     public Color color;
-    public int type;
+    public char type;
 
-    public Vertex(double x_, double y_, int type_, Color color_) {
-        this.x = x_;
-        this.y = y_;
-        this.type = type_;
-        this.color = color_;
+    public Vertex(double x_, double y_, char type_, Color color_) {
+        x = x_;
+        y = y_;
+        type = type_;
+        color = color_;
     }
+
+    public Shape createShape() {
+        Shape s = null;
+        switch (type) {
+            case 'C': // circulo
+                s = new Circle(x, y, 25);
+                break;
+            case 'Q': // quadrado
+                s = new Rectangle(x-25, y-25, 50, 50);
+                break;
+            case 'T': // triangulo
+                s = new Polygon(new double[]{x-25, y+25, x+25, y+25, x, y-25});
+                break;
+        }
+        s.setFill(color);
+        return s;
+    }
+
 }
