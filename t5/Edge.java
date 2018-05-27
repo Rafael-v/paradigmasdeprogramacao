edc8a48c31f35e2ce4778693c90a54c1c356a7b5
Rafael Vales <rafaelvalesb@gmail.com>
2018-05-27 18:22:53 -0300

    Add aresta pontilhada

--- t5/Edge.java
+++ t5/Edge.java
@@ -2,18 +2,21 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Line;
 
 public class Edge {
-    public Vertex start;
-    public Vertex end;
-    public Color color;
+    private Vertex start;
+    private Vertex end;
+    private Color color;
+    private Boolean dotted;
 
-    public Edge(Vertex start_, Vertex end_, Color color_) {
+    public Edge(Vertex start_, Vertex end_, Boolean dotted_, Color color_) {
         start = start_;
         end = end_;
+        dotted = dotted_;
         color = color_;
     }
 
     public Line getLine() {
         Line line = new Line(start.getX(), start.getY(), end.getX(), end.getY());
+        if (dotted) line.getStrokeDashArray().addAll(25.0, 20.0, 5.0, 20.0);
         line.setStroke(color);
         return line;
     }
