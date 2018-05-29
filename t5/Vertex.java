7fff544b49360d98e994577100465c41d2ebe44a
Rafael Vales <rafaelvalesb@gmail.com>
2018-05-29 01:45:32 -0300

    Add método para verificar colisão
    Verifica se o vértice se colide com outro da lista recebida

--- t5/Vertex.java
+++ t5/Vertex.java
@@ -1,12 +1,19 @@
+import java.util.ArrayList;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.*;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class Vertex {
     private double x, y;
     private Color color;
-    private char type;
+    private int type;
 
-    public Vertex(double x_, double y_, char type_, Color color_) {
+    // set the default shape size
+    private static final double size = 25;
+
+    public Vertex(double x_, double y_, int type_, Color color_) {
         x = x_;
         y = y_;
         type = type_;
@@ -14,19 +21,20 @@ public class Vertex {
     }
 
     public Shape getShape() {
-        Shape s;
+        Shape shape = null;
+
         switch (type) {
-            case 'Q': // quadrado
-                s = new Rectangle(x-25, y-25, 50, 50);
+            case 1: // square
+                shape = new Rectangle(x-(size), y-(size), size*2, size*2);
                 break;
-            case 'T': // triangulo
-                s = new Polygon(new double[]{x-25, y+25, x+25, y+25, x, y-25});
+            case 2: // triangle
+                shape = new Polygon(new double[]{x-(size), y+(size), x+(size), y+(size), x, y-(size)});
                 break;
-            default:  // circulo como default
-                s = new Circle(x, y, 25);
+            default: // circle
+                shape = new Circle(x, y, (size));
         }
-        s.setFill(color);
-        return s;
+        shape.setFill(color);
+        return shape;
     }
 
     public double getX() {
@@ -37,15 +45,28 @@ public class Vertex {
         return y;
     }
 
-    public char getType() {
+    public int getType() {
         return type;
     }
 
-    public String getColor() {
-        String colorHex = String.format("#%02X%02X%02X", 
+    public double getSize() {
+        return size;
+    }
+
+    public String getColorHex() {
+        return String.format("#%02X%02X%02X", 
             (int)(color.getRed() * 255),
             (int)(color.getGreen() * 255),
             (int)(color.getBlue() * 255));
-        return colorHex;
+    }
+
+    public Boolean vertexCollision(ArrayList<Vertex> vertexList) {
+        for (Vertex v : vertexList) {
+            Shape intersect = Shape.intersect(this.getShape(), v.getShape());
+            if (intersect.getBoundsInLocal().getWidth() != -1) {
+                return true;
+            }
+        }
+        return false;
     }
 }
