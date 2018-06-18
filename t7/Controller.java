cd686a356faf4db058e0a3aea70c2bce1c65bf93
Rafael Vales <rafaelvalesb@gmail.com>
2018-06-17 22:45:31 -0300

    Update Controller.java

--- t7/Controller.java
+++ t7/Controller.java
@@ -1,127 +1,83 @@
-import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-
-import javafx.scene.control.Button;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.effect.DropShadow;
-
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableCell;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Stage;
-import javafx.util.*;
-import javafx.scene.chart.*;
-import javafx.scene.input.MouseEvent;
-
-import javafx.beans.property.SimpleStringProperty;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.paint.Color;
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.io.IOException;
-import java.util.Map;
 import java.util.ArrayList;
-import java.util.List;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.control.TextInputDialog;
 import java.util.Optional;
 
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.chart.XYChart;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextInputDialog;
+import javafx.scene.text.Font;
+import javafx.scene.web.WebView;
 
 public class Controller {
-    private Model model = new Model();
+    private final Model model = new Model();
 
-    public ArrayList<Dado> obterTodasPosicoes() {
-        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterTodasPosicoes");
-    }
-
-    public ArrayList<Dado> obterPosicoesDaLinha(String linha) {
-        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDaLinha/" + linha);
-    }
-
-    public ArrayList<Dado> obterPosicoesDoOnibus(String ordem) {
-        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDoOnibus/" + ordem);
-    }
-
-    public void atualizaTextos(ArrayList<Label> list) {
-        list.get(0).setText("Ultima leitura de dados:\t" + model.getDataUltimaLeitura());
-        list.get(1).setText("Data mais recente:  \t" + model.getDataMaisRecente());
-        list.get(2).setText("Data menos recente: \t" + model.getDataMenosRecente());
+    public void atualizaTextos(ArrayList<Label> labels) {
+        labels.get(0).setText("Ultima leitura de dados:\t" + model.getDataUltimaLeitura());
+        labels.get(1).setText("Data mais recente:  \t" + model.getDataMaisRecente());
+        labels.get(2).setText("Data menos recente: \t" + model.getDataMenosRecente());
     }
 
     public void atualizaPieChart(PieChart pie) {
+        int total = model.getTotalVeiculos();
         int parados = model.getVeiculosParados();
-        int movimento = model.getTotalVeiculos() - parados;
 
+        pie.setTitle(total + " veiculos na ultima leitura");
         ObservableList<PieChart.Data> pieChartData =
                 FXCollections.observableArrayList(
                 new PieChart.Data(parados + " parado(s)", parados),
-                new PieChart.Data(movimento + " em movimento", movimento));
+                new PieChart.Data((total-parados) + " em movimento", (total-parados)));
 
         pie.setData(pieChartData);
     }
 
+    @SuppressWarnings("unchecked")
     public void atualizaBarChart(BarChart bar) {
-        XYChart.Series barras = new XYChart.Series();
+        XYChart.Series<String,Number> barras = new XYChart.Series<>();
 
         ArrayList<String> linhas = model.getLinhas();
         for (String l : linhas) {
             int veiculos = model.getVeiculosMovimentoNaLinha(l);
-            if (veiculos > 0) {
-                barras.getData().add(new XYChart.Data(l, veiculos));
-            }
+            if (veiculos > 0)
+                barras.getData().add(new XYChart.Data<>(l, veiculos));
         }
 
         bar.getData().clear();
         bar.getData().add(barras);
     }
 
-    public Button getTodasPosicoesButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> textos) {
+    public void atualizaWebView(WebView browser) {
+
+    }
+
+    public Button getTodasPosicoesButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> labels) {
         Button btn = new Button("Obter todas linhas");
-
-        btn.setStyle("-fx-font: 14 arial; -fx-base: #b6e7c9;");
-
+        btn.setFont(new Font("Arial", 14));
         
         btn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 ObservableList<Dado> observable = FXCollections.observableArrayList();
-                ArrayList<Dado> array = obterTodasPosicoes();
-                for (Dado d : array)
+                for (Dado d : obterTodasPosicoes())
                     observable.add(d);
                 table.setItems(observable);
-                atualizaTextos(textos);
+                atualizaTextos(labels);
                 atualizaPieChart(pie);
                 atualizaBarChart(bar);
-
-                for (TableColumn c : table.getColumns())
-                    System.out.println(c.getWidth());
             }
         });
 
         return btn;
     }
 
-    public Button getPosicoesDaLinhaButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> textos) {
+    public Button getPosicoesDaLinhaButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> labels) {
         Button btn = new Button("Filtrar por linha");
-        btn.setStyle("-fx-font: 14 arial; -fx-base: #b6e7c9;");
+        btn.setFont(new Font("Arial", 14));
     
         btn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
@@ -133,11 +89,10 @@ public class Controller {
                 Optional<String> result = dialog.showAndWait();
                 if (result.isPresent()) {
                     ObservableList<Dado> observable = FXCollections.observableArrayList();
-                    ArrayList<Dado> array = obterPosicoesDaLinha(result.get());
-                    for (Dado d : array)
+                    for (Dado d : obterPosicoesDaLinha(result.get()))
                         observable.add(d);
                     table.setItems(observable);
-                    atualizaTextos(textos);
+                    atualizaTextos(labels);
                     atualizaPieChart(pie);
                     atualizaBarChart(bar);
                 }
@@ -147,10 +102,10 @@ public class Controller {
         return btn;
     }
 
-    public Button getPosicoesDoOnibusButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> textos) {
+    public Button getPosicoesDoOnibusButton(TableView<Dado> table, PieChart pie, BarChart bar, ArrayList<Label> labels) {
         Button btn = new Button("Filtrar por onibus");
-        btn.setStyle("-fx-font: 14 arial; -fx-base: #b6e7c9;");
-    
+        btn.setFont(new Font("Arial", 14));
+
         btn.setOnAction(new EventHandler<ActionEvent>() {
             public void handle(ActionEvent event) {
                 TextInputDialog dialog = new TextInputDialog();
@@ -161,11 +116,10 @@ public class Controller {
                 Optional<String> result = dialog.showAndWait();
                 if (result.isPresent()) {
                     ObservableList<Dado> observable = FXCollections.observableArrayList();
-                    ArrayList<Dado> array = obterPosicoesDoOnibus(result.get());
-                    for (Dado d : array)
+                    for (Dado d : obterPosicoesDoOnibus(result.get()))
                         observable.add(d);
                     table.setItems(observable);
-                    atualizaTextos(textos);
+                    atualizaTextos(labels);
                     atualizaPieChart(pie);
                     atualizaBarChart(bar);
                 }
@@ -173,5 +127,17 @@ public class Controller {
         });
 
         return btn;
+    }
+
+    private ArrayList<Dado> obterTodasPosicoes() {
+        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterTodasPosicoes");
+    }
+
+    private ArrayList<Dado> obterPosicoesDaLinha(String linha) {
+        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDaLinha/" + linha);
+    }
+
+    private ArrayList<Dado> obterPosicoesDoOnibus(String ordem) {
+        return model.obterDados("http://dadosabertos.rio.rj.gov.br/apiTransporte/apresentacao/rest/index.cfm/obterPosicoesDoOnibus/" + ordem);
     }
 }
