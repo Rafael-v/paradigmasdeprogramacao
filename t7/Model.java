7bf5c03f14e33e4b969b1427336ec2b2eb7f31e5
Rafael Vales <rafaelvalesb@gmail.com>
2018-06-30 23:36:03 -0300

    Remove imports sem uso

--- t7/Model.java
+++ t7/Model.java
@@ -11,8 +11,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
